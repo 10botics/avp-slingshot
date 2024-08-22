775ad1fb6795784d159fff24896769f42f9aec12
71629 <105049686+71629@users.noreply.github.com>
2024-08-22 15:40:25 +0800

    Lesson 2

---
+++ Lesson 2/Assets/Script/Input/InputReader.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.EnhancedTouch;
+using UnityEngine.InputSystem.LowLevel;
+using TouchPhase = UnityEngine.InputSystem.TouchPhase;
+
+namespace Input
+{
+    public class InputReader : MonoBehaviour
+    {
+        [SerializeField] private InputActionReference touchState;
+        [SerializeField] private InputActionReference touchPhase;
+        
+        private SpatialPointerState TouchState => touchState.action.ReadValue<SpatialPointerState>();
+        private TouchPhase TouchPhase => touchPhase.action.ReadValue<TouchPhase>();
+
+        private void Awake()
+        {
+            EnhancedTouchSupport.Enable();
+            touchState.action.Enable();
+            touchPhase.action.Enable();
+        }
+
+        private void Update()
+        {
+            switch (TouchPhase)
+            {
+                case TouchPhase.Began:
+                    Debug.Log("Started");
+                    break;
+                case TouchPhase.Moved:
+                    Debug.Log("Moved");
+                    break;
+                case TouchPhase.Ended:
+                    Debug.Log("Ended");
+                    break;
+            }
+        }
+
+        private void OnDestroy()
+        {
+            touchState.action.Disable();
+            touchPhase.action.Disable();
+            EnhancedTouchSupport.Disable();
+        }
+    }
+}
